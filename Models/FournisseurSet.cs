0f12085a55cf428bcd9c7c59186d230b53985b91
yassin mouja <yassin_momo_villa@hotmail.fr>
2021-05-28 01:29:03 +0100

    VALIDATION FOURNISSEUR

--- Models/FournisseurSet.cs
+++ Models/FournisseurSet.cs
@@ -21,31 +21,44 @@ namespace Projet_Onssa_Web_Mvc.Models
         [Key]
         public int IdFournisseur { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "veuillez fournir un nom")]
+        [Display (Name = "Nom Fournisseur")]
         public string Nom { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir Adresse")]
+        [Display(Name = "Adresse Fournisseur")]
         public string Adresse { get; set; }
 
+        [Required(ErrorMessage = "veuillez fournir RC n°")]
+        [Display(Name = "RC n°")]
         public int RC_n { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir Patente n°")]
+        [Display(Name = "Patente n°")]
         public string Patente_n { get; set; }
 
+        [Required(ErrorMessage = "veuillez fournir IF n°")]
+        [Display(Name = "IF n°")]
         public int IF_n { get; set; }
 
+        [Required(ErrorMessage = "veuillez fournir CNSS n°")]
+        [Display(Name = "CNSS n°")]
         public int CNSS_n { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir Compte bancaire n°")]
+        [Display(Name = "Compte bancaire n°")]
         public string Compte_bancaire_n { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir ICE")]
+        [Display(Name = "ICE")]
         public string ICE { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir la ville")]
+        [Display(Name = "Ville")]
         public string Ville { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "veuillez fournir Banque")]
+        [Display(Name = "Banque")]
         public string Banque { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
