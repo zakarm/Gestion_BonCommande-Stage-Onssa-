0329f3c57221bf7307172eda526854ed5b0a112d
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2021-05-28 15:21:07 +0100

    vzlider

--- Controllers/AccueilController.cs
+++ Controllers/AccueilController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web;
+using System.Web.Mvc;
+using Projet_Onssa_Web_Mvc.Models;
+
 
 namespace Projet_Onssa_Web_Mvc.Controllers
 {
@@ -11,6 +15,22 @@ namespace Projet_Onssa_Web_Mvc.Controllers
         // GET: Accueil
         public ActionResult Statistique()
         {
+            return View();
+        }
+
+        [HttpPost , ActionName("Statistique")]
+        public ActionResult Statistique_Set()
+        {
+           
+           using (Onssa_Model ctx = new Onssa_Model())
+            {
+                var query1 = from bc in ctx.BCSet
+                             where bc.DateBC.Month.Equals(DateTime.Today.Month)
+                             select bc;
+               
+                ViewBag.dpns = query1.Count().ToString();
+            }
+             
             return View();
         }
     }
