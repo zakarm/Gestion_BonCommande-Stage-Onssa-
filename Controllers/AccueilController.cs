04bee85d702caee56c611612ddf39bb4caa927a6
yassin mouja <yassin_momo_villa@hotmail.fr>
2021-05-30 17:58:19 +0100

    Stat

--- Controllers/AccueilController.cs
+++ Controllers/AccueilController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,21 +16,36 @@ namespace Projet_Onssa_Web_Mvc.Controllers
         [HttpGet]
         public ActionResult Statistique()
         {
-            return View();
-        }
-
-        [HttpPost , ActionName("Statistique")]
-        public ActionResult Statistique_Set()
-        {
-           
-           
-             var query1 = from bc in ctx.BCSet
+            //
+            var query1 = from bc in ctx.BCSet
                          where bc.DateBC.Month.Equals(DateTime.Today.Month)
                          select bc;
-               
-            ViewBag.Dpns = query1.ToList().Count.ToString() ;
-            
+            ViewBag.Dpns =query1.ToList().Count.ToString();
+
+            //
+            var data = from bc in ctx.BCSet
+                       join pvj in ctx.PVJSet on bc.PVJSet.IdPVJ equals pvj.IdPVJ
+                       join m in ctx.ModeleDevisSet on pvj.FournisseurSet.IdFournisseur equals
+                       m.FournisseurSet.IdFournisseur
+                       where m.ConsultationSet.IdConsultation == pvj.ConsultationSet.IdConsultation && bc.DateBC.Month.Equals(DateTime.Today.Month)
+                       orderby bc.DateBC.Month ascending
+                       select m;
+            ViewBag.t=data.AsEnumerable().Sum(o => o.Ttc);
+
+            //Fournisseur li repondaw had chhar 
+            using(SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-KCK4EL9\SQLEXPRESS;Initial Catalog=Onssa_Projet;Integrated Security=True"))
+            {
+                SqlCommand cmd = new SqlCommand("select count(*) from FournisseurSet f inner join PVJSet pvj on f.IdFournisseur = pvj.InfoFournisseur_IdFournisseur inner join BCSet bc on pvj.IdPVJ = bc.IdBC where f.IdFournisseur in (select distinct pf.ListFournisseursRepondu_IdFournisseur from PVJFournisseur pf ) and month(bc.DateBC) = MONTH( GETDATE())", con);
+                con.Open();
+                string count = cmd.ExecuteScalar().ToString();
+                con.Close();
+
+                ViewBag.count = count;
+            }
+
+
             return View();
         }
+       
     }
 }
