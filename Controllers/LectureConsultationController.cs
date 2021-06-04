46658999eeccf9cc27d0213cf38d58c54b4fbbc6
Zakariae Mrabet <zakariaemrabet1@gmail.com>
2021-06-04 21:37:43 +0100

    valider

--- Controllers/LectureConsultationController.cs
+++ Controllers/LectureConsultationController.cs
@@ -7,6 +7,12 @@ using System.Data;
 using System.Data.Entity;
 using System.Net;
 using Projet_Onssa_Web_Mvc.Models;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace Projet_Onssa_Web_Mvc.Views
 {
@@ -15,16 +21,31 @@ namespace Projet_Onssa_Web_Mvc.Views
         // GET: LectureConsultation
 
         private Onssa_ProjetEntities db = new Onssa_ProjetEntities();
+
+        DataSetReport ds = new DataSetReport();
+
+        Models.DataSetReportTableAdapters.ConsultationSetTableAdapter dac = new Models.DataSetReportTableAdapters.ConsultationSetTableAdapter();
+        Models.DataSetReportTableAdapters.ConsultationFournisseurTableAdapter dacf = new Models.DataSetReportTableAdapters.ConsultationFournisseurTableAdapter();
+        Models.DataSetReportTableAdapters.FournisseurSetTableAdapter daf = new Models.DataSetReportTableAdapters.FournisseurSetTableAdapter();
+
         public ActionResult LectureConsultation()
         {
             ViewBag.Cons = new SelectList(db.ConsultationSet, "IdConsultation", "NumConsultation");
             return View();
         }
 
-        public ActionResult RempCombo()
+
+        public ActionResult GetFList(int? IdCOn)
         {
-            ViewBag.Four = new SelectList(db.ConsultationSet, "IdConsultation", "NumConsultation");
+
+            var query = from f in ds.FournisseurSet
+                        join c in ds.ConsultationFournisseur
+                        on f.IdFournisseur equals c.ListFournisseur_IdFournisseur
+                        where c.ListConsultation_IdConsultation == IdCOn
+                        select f;
+            ViewBag.Four = new SelectList(query.ToList(), "IdFournisseur", "Nom");
             return View();
+
         }
     }
 }
